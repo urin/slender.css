01b186936484ebbf57660005ec8e977a638db7bb
urin <urinkun@gmail.com>
2021-02-11 22:37:03 +0900

    Highlight menu of document by IntersectionObserver

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,45 +1,54 @@
-/* global Vue, hljs */
-document.location.hash = document.location.hash || '#overview'
+/* global Vue, hljs, IntersectionObserver */
+const store = {
+  hash: null,
+  topic: null,
+  color: {
+    h: +cssvar('--h'),
+    s: +cssvar('--s'),
+    l: +cssvar('--l')
+  },
+  bg: {
+    h: +cssvar('--bg-h'),
+    s: +cssvar('--bg-s'),
+    l: +cssvar('--bg-l')
+  }
+}
+
+function cssvar (name, value) {
+  if (value == null) {
+    return window.getComputedStyle(document.documentElement).getPropertyValue(name)
+  }
+  document.documentElement.style.setProperty(name, value)
+  return document.documentElement.style
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  const vm = new Vue({
-    el: 'main',
-    data: {
-      hash: null,
-      color: {
-        h: +cssvar('--h'),
-        s: +cssvar('--s'),
-        l: +cssvar('--l')
-      },
-      bg: {
-        h: +cssvar('--bg-h'),
-        s: +cssvar('--bg-s'),
-        l: +cssvar('--bg-l')
-      }
-    },
-    watch: {
-      'color.h': val => cssvar('--h', val),
-      'color.s': val => cssvar('--s', val),
-      'color.l': val => cssvar('--l', val),
-      'bg.h': val => cssvar('--bg-h', val),
-      'bg.s': val => cssvar('--bg-s', val),
-      'bg.l': val => cssvar('--bg-l', val)
-    },
-    methods: {
-      hashchange: function () {
-        this.hash = document.location.hash
-      }
-    }
-  })
-  window.addEventListener('hashchange', function () { vm.hashchange() })
   hljs.initHighlighting()
-  vm.hashchange()
+  ;(
+    new Vue({
+      data: store,
+      watch: {
+        'color.h': val => cssvar('--h', val),
+        'color.s': val => cssvar('--s', val),
+        'color.l': val => cssvar('--l', val),
+        'bg.h': val => cssvar('--bg-h', val),
+        'bg.s': val => cssvar('--bg-s', val),
+        'bg.l': val => cssvar('--bg-l', val)
+      }
+    })
+  ).$mount('main')
 
-  function cssvar (name, value) {
-    if (value == null) {
-      return window.getComputedStyle(document.documentElement).getPropertyValue(name)
-    }
-    document.documentElement.style.setProperty(name, value)
-    return document.documentElement.style
-  }
+  const menuObserver = new IntersectionObserver(
+    entries => {
+      if (!entries.length) { return }
+      store.topic = entries.reduce(
+        (topic, e) => e.isIntersecting ? e.target.id : topic,
+        store.topic
+      )
+    },
+    { rootMargin: '-50% 0px' }
+  )
+  document.querySelectorAll('section').forEach(target => {
+    menuObserver.observe(target)
+  })
 })
